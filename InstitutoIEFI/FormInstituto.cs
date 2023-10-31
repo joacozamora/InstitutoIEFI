6811cfdb3d9327802ba213470d128fc28e37f25e
Joaquin <joacozamoracab@gmail.com>
2023-10-31 15:26:49 -0300

    Primer commit IEFI

---
+++ InstitutoIEFI/FormInstituto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstitutoIEFI
+{
+	public partial class FormInstituto : Form
+	{
+		public FormInstituto()
+		{
+			InitializeComponent();
+		}
+	}
+}
